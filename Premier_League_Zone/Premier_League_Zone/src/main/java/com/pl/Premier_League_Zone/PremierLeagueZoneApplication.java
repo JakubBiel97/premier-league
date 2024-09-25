7c44097995bc2a2c3b5f2a9fff277545a6926bd9
Jakub Biel <jakubbiel97@gmail.com>
2024-09-25 22:15:36 +0200

    Initial SpringBoot project created

---
+++ Premier_League_Zone/Premier_League_Zone/src/main/java/com/pl/Premier_League_Zone/PremierLeagueZoneApplication.java
@@ -0,0 +1,13 @@
+package com.pl.Premier_League_Zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierLeagueZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierLeagueZoneApplication.class, args);
+	}
+//ciekawe co to
+}
---
+++ Premier_League_Zone/Premier_League_Zone/src/test/java/com/pl/Premier_League_Zone/PremierLeagueZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.Premier_League_Zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierLeagueZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
